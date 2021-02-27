a806ed0f4ddfd679ee2dd865222d556db8ba67a9
rodnavarroc <rodolfo.0926@hotmail.com>
2021-02-27 03:05:23 -0600

    Added local API

--- src/App.js
+++ src/App.js
@@ -15,44 +15,7 @@ function App() {
 
         <div className="containerx" id="mainC">
 
-          <Item
-            title="TikTok reaches $92 million settlement over nationwide privacy lawsuit"
-            subTitle="Lawsuit claims TikTok misused ‘highly sensitive’ data"
-            date=" Feb 25, 2021, 2:12pm EST"
-            firstBlock="TikTok has agreed to pay $92 million to settle a class action lawsuit over alleged privacy violations, which included claims that the app collected “highly sensitive personal data” to track users and target ads to them. TikTok rejected the allegations but said it didn’t want to spend time litigating the issue."
-            secondBlock="“While we disagree with the assertions, rather than go through lengthy litigation, we’d like to focus our efforts on building a safe and joyful experience for the TikTok community,” a TikTok spokesperson wrote in a statement sent to The Verge."
-          />
-
-          <Item
-            title="Xbox Live is down with users unable to sign in"
-            subTitle="Xbox Live sign-in is down worldwide"
-            date=" Feb 25, 2021, 3:18pm EST"
-            firstBlock="Microsoft’s Xbox Live service is down, preventing users from signing in to the service. The Xbox Live problems started at around 3:15PM ET today, and Microsoft has confirmed it’s investigating the outage. “We are aware that users may not be able to sign-in to Xbox Live at this time,” says Xbox support. “Our teams are currently investigating to fix this issue.”"
-            secondBlock="If you’re not already signed in to your Xbox, the sign-in process will fail with an error, preventing access to certain services like Xbox Party chat. Some apps may also fail to launch without the core Xbox Live services being online, and a number of games will also not launch correctly."
-          />
-
-          <Item
-            title="Final Fantasy VII Ever Crisis compiles the entire FFVII timeline into one mobile game"
-            subTitle="Coming in 2022 for iOS and Android"
-            date="Feb 25, 2021, 6:23pm EST"
-            firstBlock="Square Enix announced two mobile games set in the Final Fantasy VII universe today. One is a battle royale game aiming for a 2021 release, and the other is Final Fantasy VII Ever Crisis, a compilation of all the games and stories set in the Final Fantasy VII universe (Final Fantasy VII, Advent Children, Before Crisis, Crisis Core, and Dirge of Cerberus). The latter is slated to launch sometime in 2022 on Android and iOS. "
-          />
-
-          <Item
-            title="AT&T spins out DirecTV as a $16B company, a quarter of what it paid in 2015"
-            subTitle="Existing deals like NFL Sunday Ticket will carry over to the new company"
-            date="Feb 25, 2021, 5:33pm EST "
-            firstBlock="AT&T today announced plans to spin out its various video businesses — DirecTV, AT&T TV, and U-Verse — into a new company that it will jointly control with TPG Capital. The deal sees AT&T holding on to a 70 percent stake in the company, with TPG getting the other 30. “The transaction to separate AT&T’s US video business into New DirecTV implies an enterprise value for the new company of $16.25 billion.” "
-            secondBlock="AT&T paid a total of $67 billion to buy DirecTV: it landed the satellite provider for $48.5 billion in 2014, and the rest was debt that AT&T took on."
-          />
-
-          <Item
-            title="Twitch, owned by Amazon, pulls Amazon’s anti-union ads"
-            subTitle="Twitch says the ads from its parent company ‘should never have been allowed to run’"
-            date=" Feb 25, 2021, 2:46pm EST"
-            firstBlock="Twitch is removing the anti-union ads that its parent company, Amazon, was running on the platform. The ads showed Amazon employees talking about why they want to vote no on unionization and directed viewers to Amazon’s “DoItWithoutDues” website. A Twitch spokesperson said the ads “should never have been allowed to run on [the] service,” as they violate its political advertising policies. "
-            secondBlock="Twitch also says that it is “evaluating [its] review processes to ensure that similar content does not run in the future.”"
-          />
+         <Item />
 
         </div>
 
--- src/Item.js
+++ src/Item.js
@@ -1,19 +1,37 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
 
-function Item(props) {
-    return (
-        <div className="item">
-              <div className="container">
-                <div className="row">
-                  <div className="col-12 articleTitle pb-2">{props.title}</div>
-                  <div className="col-12 articleSubtitle pb-3">{props.subTitle}</div>
-                  <div className="col-12 articleDate pb-3">{props.date} <br/>ver.ge/5MbXo3</div>
-                  <div className="col-12 bg-light text-dark p-3 articleBody">{props.firstBlock}</div>
-                  <div className="col-12 bg-light text-dark p-3 articleBody">{props.secondBlock}</div>
-                </div>
-              </div>
+function Item() {
+  const [isLoading, setLoading] = useState(true);
+  const [articles, setArticles] = useState();
+
+  useEffect(() => {
+    axios.get("http://192.168.15.44:5000/theverge/tech").then(response => {
+      setArticles(response.data);
+      setLoading(false);
+    });
+  }, []);
+
+  if (isLoading) {
+    return <div className="loadingTitle">Loading latest news...</div>;
+  }
+
+  return (
+    articles.map(function (article, index) {
+      return (
+        <div className="item" key={index}>
+          <div className="container">
+            <div className="row">
+              <div className="col-12 articleTitle pb-2">{article.title}</div>
+              <div className="col-12 articleSubtitle pb-3">{article.subTitle}</div>
+              <div className="col-12 articleDate pb-3">{article.date} <br/> <a href={article.url} className="readFull" target="_blank" rel="noopener noreferrer">Read full article on The Verge</a></div>
+              <div className="col-12 bg-light text-dark p-3 articleBody">{article.content}</div>
+            </div>
           </div>
-    );
+        </div>
+      )
+    })
+  );
 }
 
-export default Item
+export default Item;
